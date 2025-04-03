8c2243a9457c56b847f103a4de54f1398d690522
georgeailenei <george.ailenei@gmail.com>
2025-04-03 10:28:57 +0300

    installed axios and fetched the data provided

--- src/pages/SelectSkip/SelectSkip.tsx
+++ src/pages/SelectSkip/SelectSkip.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "../../components/Card/Card";
-import './SelectSkipStyles.css';
+import "./SelectSkipStyles.css";
 
 const SelectSkip = () => {
+  const [skipOptions, setSkipOptions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
+        );
+        setSkipOptions(response.data);
+      } catch (error) {
+        throw new Error(`${error}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(skipOptions);
+
   return (
     <div>
-      <Card />
+        <Card />
     </div>
-  )
-}
+  );
+};
 
 export default SelectSkip;
